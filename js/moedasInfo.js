7c734c7f7890f5893c867bc08c2938a2ab4e5c7f
Luiz Fernando <luizfernando@exitlag.com>
2025-04-24 11:41:08 -0300

    FIRST COMMIT

---
+++ js/moedasInfo.js
@@ -0,0 +1,962 @@
+const moedasInfo = {
+  "USD": {
+    "nome": "Estados Unidos",
+    "simbolo": "$",
+    "codigoPais": "us",
+    "continente": "América do Norte"
+  },
+  "EUR": {
+    "nome": "União Europeia",
+    "simbolo": "€",
+    "codigoPais": "eu",
+    "continente": "Europa"
+  },
+  "BRL": {
+    "nome": "Brasil",
+    "simbolo": "R$",
+    "codigoPais": "br",
+    "continente": "América do Sul"
+  },
+  "JPY": {
+    "nome": "Japão",
+    "simbolo": "¥",
+    "codigoPais": "jp",
+    "continente": "Ásia"
+  },
+  "GBP": {
+    "nome": "Reino Unido",
+    "simbolo": "£",
+    "codigoPais": "gb",
+    "continente": "Europa"
+  },
+  "AUD": {
+    "nome": "Austrália",
+    "simbolo": "A$",
+    "codigoPais": "au",
+    "continente": "Oceania"
+  },
+  "CAD": {
+    "nome": "Canadá",
+    "simbolo": "C$",
+    "codigoPais": "ca",
+    "continente": "América do Norte"
+  },
+  "CHF": {
+    "nome": "Suíça",
+    "simbolo": "CHF",
+    "codigoPais": "ch",
+    "continente": "Europa"
+  },
+  "CNY": {
+    "nome": "China",
+    "simbolo": "¥",
+    "codigoPais": "cn",
+    "continente": "Ásia"
+  },
+  "SEK": {
+    "nome": "Suécia",
+    "simbolo": "kr",
+    "codigoPais": "se",
+    "continente": "Europa"
+  },
+  "NZD": {
+    "nome": "Nova Zelândia",
+    "simbolo": "NZ$",
+    "codigoPais": "nz",
+    "continente": "Oceania"
+  },
+  "MXN": {
+    "nome": "México",
+    "simbolo": "$",
+    "codigoPais": "mx",
+    "continente": "América do Norte"
+  },
+  "SGD": {
+    "nome": "Singapura",
+    "simbolo": "S$",
+    "codigoPais": "sg",
+    "continente": "Ásia"
+  },
+  "HKD": {
+    "nome": "Hong Kong",
+    "simbolo": "HK$",
+    "codigoPais": "hk",
+    "continente": "Ásia"
+  },
+  "NOK": {
+    "nome": "Noruega",
+    "simbolo": "kr",
+    "codigoPais": "no",
+    "continente": "Europa"
+  },
+  "KRW": {
+    "nome": "Coreia do Sul",
+    "simbolo": "₩",
+    "codigoPais": "kr",
+    "continente": "Ásia"
+  },
+  "TRY": {
+    "nome": "Turquia",
+    "simbolo": "₺",
+    "codigoPais": "tr",
+    "continente": "Ásia"
+  },
+  "RUB": {
+    "nome": "Rússia",
+    "simbolo": "₽",
+    "codigoPais": "ru",
+    "continente": "Europa"
+  },
+  "INR": {
+    "nome": "Índia",
+    "simbolo": "₹",
+    "codigoPais": "in",
+    "continente": "Ásia"
+  },
+  "ZAR": {
+    "nome": "África do Sul",
+    "simbolo": "R",
+    "codigoPais": "za",
+    "continente": "África"
+  },
+  "PDH": {
+    "nome": "PDH",
+    "simbolo": "PDH",
+    "codigoPais": "pd",
+    "continente": "África"
+  },
+  "LIP": {
+    "nome": "Liechtenstein",
+    "simbolo": "LIP",
+    "codigoPais": "li",
+    "continente": "África"
+  },
+  "XLO": {
+    "nome": "XLO",
+    "simbolo": "XLO",
+    "codigoPais": "xl",
+    "continente": "Oceania"
+  },
+  "CPJ": {
+    "nome": "CPJ",
+    "simbolo": "CPJ",
+    "codigoPais": "cp",
+    "continente": "Europa"
+  },
+  "NAC": {
+    "nome": "Namíbia",
+    "simbolo": "NAC",
+    "codigoPais": "na",
+    "continente": "América do Norte"
+  },
+  "DIM": {
+    "nome": "DIM",
+    "simbolo": "DIM",
+    "codigoPais": "di",
+    "continente": "América do Sul"
+  },
+  "QFW": {
+    "nome": "QFW",
+    "simbolo": "QFW",
+    "codigoPais": "qf",
+    "continente": "África"
+  },
+  "FPY": {
+    "nome": "FPY",
+    "simbolo": "FPY",
+    "codigoPais": "fp",
+    "continente": "Europa"
+  },
+  "FEA": {
+    "nome": "FEA",
+    "simbolo": "FEA",
+    "codigoPais": "fe",
+    "continente": "Europa"
+  },
+  "ROW": {
+    "nome": "Romênia",
+    "simbolo": "ROW",
+    "codigoPais": "ro",
+    "continente": "África"
+  },
+  "YYB": {
+    "nome": "YYB",
+    "simbolo": "YYB",
+    "codigoPais": "yy",
+    "continente": "Europa"
+  },
+  "BJV": {
+    "nome": "Benim",
+    "simbolo": "BJV",
+    "codigoPais": "bj",
+    "continente": "América do Norte"
+  },
+  "DWX": {
+    "nome": "DWX",
+    "simbolo": "DWX",
+    "codigoPais": "dw",
+    "continente": "Oceania"
+  },
+  "DXV": {
+    "nome": "DXV",
+    "simbolo": "DXV",
+    "codigoPais": "dx",
+    "continente": "Ásia"
+  },
+  "BLX": {
+    "nome": "BLX",
+    "simbolo": "BLX",
+    "codigoPais": "bl",
+    "continente": "Europa"
+  },
+  "ZKR": {
+    "nome": "ZKR",
+    "simbolo": "ZKR",
+    "codigoPais": "zk",
+    "continente": "América do Sul"
+  },
+  "FYL": {
+    "nome": "FYL",
+    "simbolo": "FYL",
+    "codigoPais": "fy",
+    "continente": "América do Sul"
+  },
+  "UQS": {
+    "nome": "UQS",
+    "simbolo": "UQS",
+    "codigoPais": "uq",
+    "continente": "América do Sul"
+  },
+  "YWD": {
+    "nome": "YWD",
+    "simbolo": "YWD",
+    "codigoPais": "yw",
+    "continente": "Ásia"
+  },
+  "XGH": {
+    "nome": "XGH",
+    "simbolo": "XGH",
+    "codigoPais": "xg",
+    "continente": "Europa"
+  },
+  "BPH": {
+    "nome": "BPH",
+    "simbolo": "BPH",
+    "codigoPais": "bp",
+    "continente": "África"
+  },
+  "NSA": {
+    "nome": "NSA",
+    "simbolo": "NSA",
+    "codigoPais": "ns",
+    "continente": "Europa"
+  },
+  "LRH": {
+    "nome": "Libéria",
+    "simbolo": "LRH",
+    "codigoPais": "lr",
+    "continente": "América do Sul"
+  },
+  "IVK": {
+    "nome": "IVK",
+    "simbolo": "IVK",
+    "codigoPais": "iv",
+    "continente": "África"
+  },
+  "TDY": {
+    "nome": "Chade",
+    "simbolo": "TDY",
+    "codigoPais": "td",
+    "continente": "Oceania"
+  },
+  "XYZ": {
+    "nome": "XYZ",
+    "simbolo": "XYZ",
+    "codigoPais": "xy",
+    "continente": "América do Norte"
+  },
+  "CPD": {
+    "nome": "CPD",
+    "simbolo": "CPD",
+    "codigoPais": "cp",
+    "continente": "América do Sul"
+  },
+  "RZP": {
+    "nome": "RZP",
+    "simbolo": "RZP",
+    "codigoPais": "rz",
+    "continente": "África"
+  },
+  "OHB": {
+    "nome": "OHB",
+    "simbolo": "OHB",
+    "codigoPais": "oh",
+    "continente": "Europa"
+  },
+  "CWC": {
+    "nome": "CWC",
+    "simbolo": "CWC",
+    "codigoPais": "cw",
+    "continente": "África"
+  },
+  "EQR": {
+    "nome": "EQR",
+    "simbolo": "EQR",
+    "codigoPais": "eq",
+    "continente": "América do Norte"
+  },
+  "WJY": {
+    "nome": "WJY",
+    "simbolo": "WJY",
+    "codigoPais": "wj",
+    "continente": "Oceania"
+  },
+  "FJZ": {
+    "nome": "Fiji",
+    "simbolo": "FJZ",
+    "codigoPais": "fj",
+    "continente": "África"
+  },
+  "NBH": {
+    "nome": "NBH",
+    "simbolo": "NBH",
+    "codigoPais": "nb",
+    "continente": "África"
+  },
+  "CPX": {
+    "nome": "CPX",
+    "simbolo": "CPX",
+    "codigoPais": "cp",
+    "continente": "Ásia"
+  },
+  "TZZ": {
+    "nome": "Tanzânia",
+    "simbolo": "TZZ",
+    "codigoPais": "tz",
+    "continente": "Oceania"
+  },
+  "LOM": {
+    "nome": "LOM",
+    "simbolo": "LOM",
+    "codigoPais": "lo",
+    "continente": "América do Sul"
+  },
+  "IQZ": {
+    "nome": "Iraque",
+    "simbolo": "IQZ",
+    "codigoPais": "iq",
+    "continente": "Ásia"
+  },
+  "IKG": {
+    "nome": "IKG",
+    "simbolo": "IKG",
+    "codigoPais": "ik",
+    "continente": "Oceania"
+  },
+  "RHK": {
+    "nome": "RHK",
+    "simbolo": "RHK",
+    "codigoPais": "rh",
+    "continente": "América do Sul"
+  },
+  "FBK": {
+    "nome": "FBK",
+    "simbolo": "FBK",
+    "codigoPais": "fb",
+    "continente": "Ásia"
+  },
+  "JSJ": {
+    "nome": "JSJ",
+    "simbolo": "JSJ",
+    "codigoPais": "js",
+    "continente": "América do Sul"
+  },
+  "EDV": {
+    "nome": "EDV",
+    "simbolo": "EDV",
+    "codigoPais": "ed",
+    "continente": "América do Norte"
+  },
+  "NHL": {
+    "nome": "NHL",
+    "simbolo": "NHL",
+    "codigoPais": "nh",
+    "continente": "Ásia"
+  },
+  "SUB": {
+    "nome": "SUB",
+    "simbolo": "SUB",
+    "codigoPais": "su",
+    "continente": "América do Sul"
+  },
+  "ZTJ": {
+    "nome": "ZTJ",
+    "simbolo": "ZTJ",
+    "codigoPais": "zt",
+    "continente": "América do Sul"
+  },
+  "NPK": {
+    "nome": "Nepal",
+    "simbolo": "NPK",
+    "codigoPais": "np",
+    "continente": "Europa"
+  },
+  "HCR": {
+    "nome": "HCR",
+    "simbolo": "HCR",
+    "codigoPais": "hc",
+    "continente": "América do Sul"
+  },
+  "PYL": {
+    "nome": "Paraguai",
+    "simbolo": "PYL",
+    "codigoPais": "py",
+    "continente": "Oceania"
+  },
+  "UPI": {
+    "nome": "UPI",
+    "simbolo": "UPI",
+    "codigoPais": "up",
+    "continente": "África"
+  },
+  "UCY": {
+    "nome": "UCY",
+    "simbolo": "UCY",
+    "codigoPais": "uc",
+    "continente": "Oceania"
+  },
+  "GXA": {
+    "nome": "GXA",
+    "simbolo": "GXA",
+    "codigoPais": "gx",
+    "continente": "América do Sul"
+  },
+  "ZZT": {
+    "nome": "ZZT",
+    "simbolo": "ZZT",
+    "codigoPais": "zz",
+    "continente": "América do Sul"
+  },
+  "BHR": {
+    "nome": "Bahrein",
+    "simbolo": "BHR",
+    "codigoPais": "bh",
+    "continente": "América do Sul"
+  },
+  "CZH": {
+    "nome": "Chéquia",
+    "simbolo": "CZH",
+    "codigoPais": "cz",
+    "continente": "Ásia"
+  },
+  "LSH": {
+    "nome": "Lesoto",
+    "simbolo": "LSH",
+    "codigoPais": "ls",
+    "continente": "América do Norte"
+  },
+  "LYS": {
+    "nome": "Líbia",
+    "simbolo": "LYS",
+    "codigoPais": "ly",
+    "continente": "América do Norte"
+  },
+  "ZGT": {
+    "nome": "ZGT",
+    "simbolo": "ZGT",
+    "codigoPais": "zg",
+    "continente": "Ásia"
+  },
+  "TAR": {
+    "nome": "TAR",
+    "simbolo": "TAR",
+    "codigoPais": "ta",
+    "continente": "Europa"
+  },
+  "WUV": {
+    "nome": "WUV",
+    "simbolo": "WUV",
+    "codigoPais": "wu",
+    "continente": "Europa"
+  },
+  "QSI": {
+    "nome": "QSI",
+    "simbolo": "QSI",
+    "codigoPais": "qs",
+    "continente": "América do Sul"
+  },
+  "ZIB": {
+    "nome": "ZIB",
+    "simbolo": "ZIB",
+    "codigoPais": "zi",
+    "continente": "América do Sul"
+  },
+  "ZUW": {
+    "nome": "ZUW",
+    "simbolo": "ZUW",
+    "codigoPais": "zu",
+    "continente": "África"
+  },
+  "HLU": {
+    "nome": "HLU",
+    "simbolo": "HLU",
+    "codigoPais": "hl",
+    "continente": "Oceania"
+  },
+  "TPN": {
+    "nome": "TPN",
+    "simbolo": "TPN",
+    "codigoPais": "tp",
+    "continente": "Oceania"
+  },
+  "XPW": {
+    "nome": "XPW",
+    "simbolo": "XPW",
+    "codigoPais": "xp",
+    "continente": "Europa"
+  },
+  "PVT": {
+    "nome": "PVT",
+    "simbolo": "PVT",
+    "codigoPais": "pv",
+    "continente": "Ásia"
+  },
+  "BGA": {
+    "nome": "Bulgária",
+    "simbolo": "BGA",
+    "codigoPais": "bg",
+    "continente": "América do Norte"
+  },
+  "VVW": {
+    "nome": "VVW",
+    "simbolo": "VVW",
+    "codigoPais": "vv",
+    "continente": "Oceania"
+  },
+  "PFG": {
+    "nome": "PFG",
+    "simbolo": "PFG",
+    "codigoPais": "pf",
+    "continente": "América do Norte"
+  },
+  "FFN": {
+    "nome": "FFN",
+    "simbolo": "FFN",
+    "codigoPais": "ff",
+    "continente": "Ásia"
+  },
+  "HPR": {
+    "nome": "HPR",
+    "simbolo": "HPR",
+    "codigoPais": "hp",
+    "continente": "América do Sul"
+  },
+  "HBD": {
+    "nome": "HBD",
+    "simbolo": "HBD",
+    "codigoPais": "hb",
+    "continente": "América do Norte"
+  },
+  "DBC": {
+    "nome": "DBC",
+    "simbolo": "DBC",
+    "codigoPais": "db",
+    "continente": "América do Norte"
+  },
+  "HHW": {
+    "nome": "HHW",
+    "simbolo": "HHW",
+    "codigoPais": "hh",
+    "continente": "América do Norte"
+  },
+  "IZD": {
+    "nome": "IZD",
+    "simbolo": "IZD",
+    "codigoPais": "iz",
+    "continente": "Ásia"
+  },
+  "VGF": {
+    "nome": "VGF",
+    "simbolo": "VGF",
+    "codigoPais": "vg",
+    "continente": "Oceania"
+  },
+  "SWH": {
+    "nome": "SWH",
+    "simbolo": "SWH",
+    "codigoPais": "sw",
+    "continente": "América do Norte"
+  },
+  "AHL": {
+    "nome": "AHL",
+    "simbolo": "AHL",
+    "codigoPais": "ah",
+    "continente": "África"
+  },
+  "APW": {
+    "nome": "APW",
+    "simbolo": "APW",
+    "codigoPais": "ap",
+    "continente": "Oceania"
+  },
+  "LVH": {
+    "nome": "Letônia",
+    "simbolo": "LVH",
+    "codigoPais": "lv",
+    "continente": "Europa"
+  },
+  "DQO": {
+    "nome": "DQO",
+    "simbolo": "DQO",
+    "codigoPais": "dq",
+    "continente": "Europa"
+  },
+  "JCX": {
+    "nome": "JCX",
+    "simbolo": "JCX",
+    "codigoPais": "jc",
+    "continente": "Europa"
+  },
+  "MGY": {
+    "nome": "Madagáscar",
+    "simbolo": "MGY",
+    "codigoPais": "mg",
+    "continente": "Europa"
+  },
+  "MNC": {
+    "nome": "Mongólia",
+    "simbolo": "MNC",
+    "codigoPais": "mn",
+    "continente": "África"
+  },
+  "BXW": {
+    "nome": "BXW",
+    "simbolo": "BXW",
+    "codigoPais": "bx",
+    "continente": "Oceania"
+  },
+  "MSZ": {
+    "nome": "MSZ",
+    "simbolo": "MSZ",
+    "codigoPais": "ms",
+    "continente": "Ásia"
+  },
+  "LQJ": {
+    "nome": "LQJ",
+    "simbolo": "LQJ",
+    "codigoPais": "lq",
+    "continente": "América do Sul"
+  },
+  "WIN": {
+    "nome": "WIN",
+    "simbolo": "WIN",
+    "codigoPais": "wi",
+    "continente": "América do Sul"
+  },
+  "QXZ": {
+    "nome": "QXZ",
+    "simbolo": "QXZ",
+    "codigoPais": "qx",
+    "continente": "América do Sul"
+  },
+  "SJQ": {
+    "nome": "SJQ",
+    "simbolo": "SJQ",
+    "codigoPais": "sj",
+    "continente": "América do Norte"
+  },
+  "QBM": {
+    "nome": "QBM",
+    "simbolo": "QBM",
+    "codigoPais": "qb",
+    "continente": "América do Sul"
+  },
+  "XQA": {
+    "nome": "XQA",
+    "simbolo": "XQA",
+    "codigoPais": "xq",
+    "continente": "Europa"
+  },
+  "ZVK": {
+    "nome": "ZVK",
+    "simbolo": "ZVK",
+    "codigoPais": "zv",
+    "continente": "África"
+  },
+  "DHC": {
+    "nome": "DHC",
+    "simbolo": "DHC",
+    "codigoPais": "dh",
+    "continente": "África"
+  },
+  "UHE": {
+    "nome": "UHE",
+    "simbolo": "UHE",
+    "codigoPais": "uh",
+    "continente": "Ásia"
+  },
+  "LZM": {
+    "nome": "LZM",
+    "simbolo": "LZM",
+    "codigoPais": "lz",
+    "continente": "Oceania"
+  },
+  "AXN": {
+    "nome": "AXN",
+    "simbolo": "AXN",
+    "codigoPais": "ax",
+    "continente": "América do Sul"
+  },
+  "SET": {
+    "nome": "Suécia",
+    "simbolo": "SET",
+    "codigoPais": "se",
+    "continente": "América do Norte"
+  },
+  "PLE": {
+    "nome": "Polônia",
+    "simbolo": "PLE",
+    "codigoPais": "pl",
+    "continente": "Europa"
+  },
+  "JSL": {
+    "nome": "JSL",
+    "simbolo": "JSL",
+    "codigoPais": "js",
+    "continente": "África"
+  },
+  "XCM": {
+    "nome": "XCM",
+    "simbolo": "XCM",
+    "codigoPais": "xc",
+    "continente": "África"
+  },
+  "LFM": {
+    "nome": "LFM",
+    "simbolo": "LFM",
+    "codigoPais": "lf",
+    "continente": "América do Sul"
+  },
+  "NPD": {
+    "nome": "Nepal",
+    "simbolo": "NPD",
+    "codigoPais": "np",
+    "continente": "América do Sul"
+  },
+  "XWO": {
+    "nome": "XWO",
+    "simbolo": "XWO",
+    "codigoPais": "xw",
+    "continente": "Oceania"
+  },
+  "RCI": {
+    "nome": "RCI",
+    "simbolo": "RCI",
+    "codigoPais": "rc",
+    "continente": "América do Norte"
+  },
+  "ECJ": {
+    "nome": "Equador",
+    "simbolo": "ECJ",
+    "codigoPais": "ec",
+    "continente": "América do Sul"
+  },
+  "BZX": {
+    "nome": "Belize",
+    "simbolo": "BZX",
+    "codigoPais": "bz",
+    "continente": "América do Norte"
+  },
+  "QFO": {
+    "nome": "QFO",
+    "simbolo": "QFO",
+    "codigoPais": "qf",
+    "continente": "América do Sul"
+  },
+  "SIN": {
+    "nome": "Eslovênia",
+    "simbolo": "SIN",
+    "codigoPais": "si",
+    "continente": "América do Norte"
+  },
+  "OLT": {
+    "nome": "OLT",
+    "simbolo": "OLT",
+    "codigoPais": "ol",
+    "continente": "Ásia"
+  },
+  "EXC": {
+    "nome": "EXC",
+    "simbolo": "EXC",
+    "codigoPais": "ex",
+    "continente": "Oceania"
+  },
+  "HVZ": {
+    "nome": "HVZ",
+    "simbolo": "HVZ",
+    "codigoPais": "hv",
+    "continente": "Europa"
+  },
+  "HTG": {
+    "nome": "Haiti",
+    "simbolo": "HTG",
+    "codigoPais": "ht",
+    "continente": "África"
+  },
+  "QJJ": {
+    "nome": "QJJ",
+    "simbolo": "QJJ",
+    "codigoPais": "qj",
+    "continente": "Europa"
+  },
+  "RQF": {
+    "nome": "RQF",
+    "simbolo": "RQF",
+    "codigoPais": "rq",
+    "continente": "Ásia"
+  },
+  "PRU": {
+    "nome": "PRU",
+    "simbolo": "PRU",
+    "codigoPais": "pr",
+    "continente": "América do Sul"
+  },
+  "GBD": {
+    "nome": "Reino Unido",
+    "simbolo": "GBD",
+    "codigoPais": "gb",
+    "continente": "África"
+  },
+  "BZJ": {
+    "nome": "Belize",
+    "simbolo": "BZJ",
+    "codigoPais": "bz",
+    "continente": "Ásia"
+  },
+  "KUU": {
+    "nome": "KUU",
+    "simbolo": "KUU",
+    "codigoPais": "ku",
+    "continente": "Ásia"
+  },
+  "XVA": {
+    "nome": "XVA",
+    "simbolo": "XVA",
+    "codigoPais": "xv",
+    "continente": "Ásia"
+  },
+  "JHN": {
+    "nome": "JHN",
+    "simbolo": "JHN",
+    "codigoPais": "jh",
+    "continente": "Ásia"
+  },
+  "DNJ": {
+    "nome": "DNJ",
+    "simbolo": "DNJ",
+    "codigoPais": "dn",
+    "continente": "África"
+  },
+  "LGE": {
+    "nome": "LGE",
+    "simbolo": "LGE",
+    "codigoPais": "lg",
+    "continente": "Oceania"
+  },
+  "OLP": {
+    "nome": "OLP",
+    "simbolo": "OLP",
+    "codigoPais": "ol",
+    "continente": "América do Sul"
+  },
+  "HGS": {
+    "nome": "HGS",
+    "simbolo": "HGS",
+    "codigoPais": "hg",
+    "continente": "Oceania"
+  },
+  "ENQ": {
+    "nome": "ENQ",
+    "simbolo": "ENQ",
+    "codigoPais": "en",
+    "continente": "Europa"
+  },
+  "VZX": {
+    "nome": "VZX",
+    "simbolo": "VZX",
+    "codigoPais": "vz",
+    "continente": "Ásia"
+  },
+  "AWD": {
+    "nome": "AWD",
+    "simbolo": "AWD",
+    "codigoPais": "aw",
+    "continente": "América do Norte"
+  },
+  "ABE": {
+    "nome": "ABE",
+    "simbolo": "ABE",
+    "codigoPais": "ab",
+    "continente": "Oceania"
+  },
+  "BVV": {
+    "nome": "BVV",
+    "simbolo": "BVV",
+    "codigoPais": "bv",
+    "continente": "América do Sul"
+  },
+  "SNC": {
+    "nome": "Senegal",
+    "simbolo": "SNC",
+    "codigoPais": "sn",
+    "continente": "Ásia"
+  },
+  "ECT": {
+    "nome": "Equador",
+    "simbolo": "ECT",
+    "codigoPais": "ec",
+    "continente": "América do Norte"
+  },
+  "ARB": {
+    "nome": "Argentina",
+    "simbolo": "ARB",
+    "codigoPais": "ar",
+    "continente": "África"
+  },
+  "SGY": {
+    "nome": "Singapura",
+    "simbolo": "SGY",
+    "codigoPais": "sg",
+    "continente": "América do Sul"
+  },
+  "TOD": {
+    "nome": "Tonga",
+    "simbolo": "TOD",
+    "codigoPais": "to",
+    "continente": "América do Norte"
+  },
+  "OPA": {
+    "nome": "OPA",
+    "simbolo": "OPA",
+    "codigoPais": "op",
+    "continente": "África"
+  },
+  "IZE": {
+    "nome": "IZE",
+    "simbolo": "IZE",
+    "codigoPais": "iz",
+    "continente": "América do Sul"
+  },
+  "LRW": {
+    "nome": "Libéria",
+    "simbolo": "LRW",
+    "codigoPais": "lr",
+    "continente": "Oceania"
+  },
+  "XEW": {
+    "nome": "XEW",
+    "simbolo": "XEW",
+    "codigoPais": "xe",
+    "continente": "América do Sul"
+  }
+};
